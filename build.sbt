59ab8b10cc2bf37fcdafbf24c74efa06b88c3b90
chrisjwwalker <cjw@walker54.plus.com>
2019-08-14 16:45:53 +0100

    OPS-18: Upgraded to support play 2.7 and scala 2.13

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2018 CJWW Development
+ *  Copyright 2019 CJWW Development
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
--- src/test/scala/com/cjwwdev/testing/integration/WireMockSetupSpec.scala
+++ src/test/scala/com/cjwwdev/testing/integration/WireMockSetupSpec.scala
@@ -1,7 +1,6 @@
 
 /*
- * Copyright 2018 CJWW Development
- *
+ * Copyright 2019 CJWW Development
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
